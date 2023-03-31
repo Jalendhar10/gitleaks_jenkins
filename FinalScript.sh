372d88e8afb076d1f6d6afa25ae5fe9939c1ef13
gem-abhay <128676796+gem-abhay@users.noreply.github.com>
2023-03-31 12:37:39 +0530

    Update FinalScript.sh

--- FinalScript.sh
+++ FinalScript.sh
@@ -20,16 +20,4 @@ do
   report_file="$reports_dir/$repo_name.json"
   cd /home/tools/gitleaks
  ./gitleaks detect -s "$repo_path" -r "$report_file" 
-
-  # Archive the generated report
-  archive_file="$repo_name.json"
-  archive_path="$reports_dir/$archive_file"
-  archive_dir=$(basename "$reports_dir")
-  cd "$reports_dir"
-  zip -r "$archive_file.zip" "$repo_name.json"
-  cd -
-  archive_path="$reports_dir/$archive_file.zip"
-  archive_dir_path="$archive_dir/$archive_file.zip"
-  echo "Archiving $archive_dir_path"
-  archiveArtifacts "$archive_path"  
 done
